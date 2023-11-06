8c17e2c730a9483beafc50df16d7c352c1cab92c
Trevor Harmon <tharmon@laspositascollege.edu>
2023-11-06 11:15:52 -0800

    Add solution

--- main.cpp
+++ main.cpp
@@ -8,5 +8,26 @@ using namespace std;
 
 int main()
 {
+	// OpenHashMap<string,int> hashtable(1000);
+	// addPopularityData(hashtable);
+	// hashtable.printGraph(50);
+	// hashtable.printStats();
+
+	// OpenHashMap<string,int> hashtable(40000, [](string key) { return 20000; });
+	// addPopularityData(hashtable);
+	// hashtable.printGraph(50);
+	// hashtable.printStats();
+
+	// OpenHashMap<string,int> hashtable(1000, [](string key) {
+	// 	int hashcode = 0;
+	// 	for (int i = 0; i < key.length(); i++) {
+    //         hashcode += 31 * key[i];
+    //     }
+	// 	return hashcode;
+	// });
+	// addPopularityData(hashtable);
+	// hashtable.printGraph(50);
+	// hashtable.printStats();
+
 	return EXIT_SUCCESS;
 }
